64a9bee3c5aa64b0e70f4f6ea93ea56e62937960
nadiyniy <140396602+nadiyniy@users.noreply.github.com>
2023-10-19 17:35:00 +0300

    update

--- src/components/friend/Friend.jsx
+++ src/components/friend/Friend.jsx
@@ -3,10 +3,7 @@ import { StyledOnlineSpan } from './Friends.style';
 export const Friend = ({ avatar, name, isOnline }) => {
   return (
     <li className="item">
-      <StyledOnlineSpan
-        $isOnline={isOnline}
-        className="status"
-      ></StyledOnlineSpan>
+      <StyledOnlineSpan $isOnline={isOnline} className="status" />
       <img className="avatar" src={avatar} alt={name} width="48" />
       <p className="name">{name}</p>
     </li>
